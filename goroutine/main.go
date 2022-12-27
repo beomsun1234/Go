09a959cfdde7721ad8b2b4fd7e06e1ec66c654cd
Beomsun <beomsun22@gmail.com>
2022-12-27 18:05:39 +0900

    goroutine, channel, select

--- goroutine/main.go
+++ goroutine/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	fmt.Println("goroutine")
+	ch1 := make(chan int)
+	ch3 := make(chan bool)
+	ch2 := make(chan string)
+	sg := &sync.WaitGroup{}
+	sg.Add(2)
+
+	go goRT1("1", ch1, ch3)
+	go goRT2(ch2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case a := <-ch1:
+			fmt.Println("ch1 = ", a, ", ch3 = ", <-ch3)
+			sg.Done()
+		case b := <-ch2:
+			fmt.Println("ch2 =", b)
+			sg.Done()
+		}
+	}
+	sg.Wait()
+	fmt.Println("-------------------------------------------")
+	fmt.Println("end")
+	close(ch1)
+	close(ch2)
+	close(ch3)
+
+}
+
+func goRT1(gubun string, ch chan int, ch2 chan bool) {
+	fmt.Println("------start goRT1------------")
+	if gubun == "1" {
+		ch <- 1
+		ch2 <- true
+	} else {
+		ch <- 0
+		ch2 <- false
+	}
+
+}
+func goRT2(ch chan string) {
+	fmt.Println("------start goRT2------------")
+	ch <- "s"
 }
